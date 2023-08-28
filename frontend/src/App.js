20c09adc6684934a1cfc7b65b8c9d8ece14b29af
Lali Bibilashvili <lalibibilashvili@gmail.com>
2023-08-28 17:26:27 +0200

    big chessboard created

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Chessboard } from "react-chessboard";
+
+function App() {
+  return (
+    <div className="App">
+      <Chessboard id="BasicBoard" />
+    </div>
+  );
+}
+
+export default App;
