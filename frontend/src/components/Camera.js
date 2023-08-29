89d5f9589b2f3a0807b0b0646d6d096de50222be
Lali Bibilashvili <lalibibilashvili@gmail.com>
2023-08-29 15:54:01 +0200

    changed camera image to png

--- frontend/src/components/Camera.js
+++ frontend/src/components/Camera.js
@@ -11,9 +11,16 @@ function Camera() {
     const imageSrc = webcamRef.current.getScreenshot();
     setImgSrc(imageSrc);
     console.log(imageSrc)
-    const buffer = imageSrc.arrayBuffer()
-    const bytesArray = new Uint8Array(buffer)
-    console.log(bytesArray)
+
+
+    // var binary_string = window.atob(imageSrc);;
+    // var len = binary_string.length;
+    // var bytes = new Uint8Array(len);
+    // for (var i = 0; i < len; i++) {
+    //     bytes[i] = binary_string.charCodeAt(i);
+    // }
+    // console.log(bytes.buffer)
+
   }, [webcamRef, imgSrc]);
 
 
@@ -23,7 +30,9 @@ function Camera() {
       {imgSrc ? (
         <img src={imgSrc} alt="webcam" />
       ) : (
-        <Webcam height={600} width={600} ref={webcamRef} />
+        <Webcam
+        height={600} width={600} ref={webcamRef}
+        screenshotFormat="image/png" />
       )}
       <div className="btn-container">
         <button onClick={()=>capture()}>Capture photo</button>
